5d1574bb0267903d5c2f3bea732249b940873c2c
LinDixuan <127588279+LinDixuan@users.noreply.github.com>
2026-04-14 22:19:49 +0000

    Deploying to gh-pages from @ LinDixuan/LinDixuan.github.io@8ad21906664e2b358d8a948d6d0e07c9566bf2b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%64%69%78%75%61%6E@%73%74%61%6E%66%6F%72%64.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/LinDixuan", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -93,14 +100,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=EgM8ybcAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
